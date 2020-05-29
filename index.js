530537259fc0e510bbcf58a904bfe21f45545e46
richCode <chineduogada@gmail.com>
2020-05-29 08:15:58 +0100

    serve static files

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((_req, _res, next) => {
   debug('Hello from the middleware.');
