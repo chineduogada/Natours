07e3985d3f2a9289950e9d0f7a612d124440b3c6
richCode <chineduogada@gmail.com>
2020-06-04 19:31:59 +0100

    feature(mongodb): connect to the database
    Create `natours` database, `tours` collection as well as two `documents`
    with the help of `mongoose`
    
    Start [#6]

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
-const debug = require('debug')('app:startup');
 
+const mongoose = require('mongoose');
+const debug = require('debug')('app:startup');
 const app = require('./index');
+
 // debug(process.env);
+
+// CONNECT MONGODB
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB)
+  .then(() => debug('successfully connected to mongodb...'))
+  .catch((ex) => debug(ex, "couldn't connect to mongodb"));
+
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'a tour must have a "name"'],
+    unique: true,
+  },
+  price: { type: Number, required: [true, 'a tour must have a "price"'] },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const tourTest = new Tour({
+  name: 'the park camper',
+  price: 997,
+});
+
+tourTest
+  .save()
+  .then((doc) => debug(doc))
+  .catch((ex) => debug(ex));
 
 // STARTS the SERVER
 const port = process.env.PORT || 8000;
