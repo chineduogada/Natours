a13e33505899f5e29e3a43075ab0f13e60833c52
richCode <chineduogada@gmail.com>
2020-06-05 22:50:38 +0100

    feature(tour): build up model
    Finish [#9]

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,11 +7,49 @@ module.exports = mongoose.model(
       type: String,
       required: [true, `a tour must have a 'name'`],
       unique: true,
+      trim: true,
     },
-    price: { type: Number, required: [true, `a tour must have a 'price'`] },
-    rating: {
+    difficulty: {
+      type: String,
+      required: [true, `a tour must have a 'difficulty'`],
+      enum: ['easy', 'medium', 'hard', 'difficult'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, `a tour must have a 'group size'`],
+    },
+    duration: {
+      type: Number,
+      required: [true, `a tour must have a 'duration'`],
+    },
+    ratingAverage: {
       type: Number,
       default: 4.5,
     },
+    ratingQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: { type: Number, required: [true, `a tour must have a 'price'`] },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, `a tour must have a 'summary'`],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, `a tour must have a 'cover image'`],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
   })
 );
