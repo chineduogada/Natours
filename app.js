27b31f0b43c11b6c54f61d6ff7789f132b884d3c
richCode <chineduogada@gmail.com>
2020-10-19 22:39:22 +0100

    add rate limit

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 const debug = require('debug')('app:startup');
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit')
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/AppError');
 const globalErrController = require('./controllers/errorController');
 
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
 const app = express();
 
+// Logging in `dev` environment
 debug(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Add Rate Limiter
+const limiter = rateLimit({
+  max: 3,
+  windowMs: 60 * 60 * 1000
+})
+
+app.use('/api', limiter)
+
+// Body parser: make `req.body` available
 app.use(express.json());
+
+// Serve Static Content
 app.use(express.static(`${__dirname}/public`));
 
-app.use((_req, _res, next) => {
-  debug('Hello from the middleware.');
-
-  next();
-});
-
+// Test middleware
 app.use((req, _res, next) => {
   req.requestTime = new Date().toDateString();
   next();
@@ -33,9 +42,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // UNHANDLED ROUTES
-app.all('*', (req, res, next) => {
+app.all('*', (req, _res, next) => {
   const err = new AppError(
-    `no such 'route: ${req.originalUrl}' on this server`,
+    `no such 'route: ${req.originalUrl}' with the 'method: ${req.method}' on this server`,
     404
   );
 
@@ -46,3 +55,11 @@ app.all('*', (req, res, next) => {
 app.use(globalErrController);
 
 module.exports = app;
+
+
+
+
+
+
+
+
